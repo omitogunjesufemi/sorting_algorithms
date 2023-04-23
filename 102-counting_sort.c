cb057d61fd63b8e611967dd00f6a68aece8caa91
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-04-23 15:03:53 +0100

    counting sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+/**
+ * counting_sort - an implementation of the counting sort
+ * on an array of integers >= 0
+ * @array: array to be sorted
+ * @size: size of the array
+ * Returns: nothing
+*/
+void counting_sort(int *array, size_t size)
+{
+	size_t i, j, count;
+	int *count_array, *sorted_array, max, number;
+
+	max = array[0];
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	count_array = malloc(sizeof(int) * (max + 1));
+	if (count_array == NULL)
+		return;
+	for (i = 0; i < (size_t)max + 1; i++)
+		count_array[i] = 0;
+	for (i = 0; i < size; i++)
+	{
+		number = array[i];
+		count = 0;
+		if (count_array[number] != 0)
+			continue;
+		for (j = 0; j < size; j++)
+		{
+			if (array[j] == number)
+				count++;
+		}
+		count_array[number] = count;
+	}
+	for (i = 0; i < (size_t)max + 1; i++)
+	{
+		if (i == 0)
+			continue;
+		count_array[i] = count_array[i] + count_array[i - 1];
+	}
+	print_array(count_array, max + 1);
+	sorted_array = malloc(sizeof(int) * size);
+	if (sorted_array == NULL)
+	{
+		free(count_array);
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		sorted_array[count_array[array[i]] - 1] = array[i];
+		count_array[array[i]]--;
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = sorted_array[i];
+	}
+	free(sorted_array);
+	free(count_array);
+}
--- sort.h
+++ sort.h
@@ -35,5 +35,6 @@ void selection_sort(int *array, size_t size);
  */
 void insertion_sort_list(listint_t **list);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif /* SORT_H */
