96f1223595771ac877aa5cd5d4f0180590ef21db
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-04-26 22:48:00 +0100

    betty

--- sort.h
+++ sort.h
@@ -64,7 +64,6 @@ void helper_function(int *array, int start_point, int end_point, int n);
  * @array: The array or sub arrays to be sorted
  * @start_point: The index pointing to the first element of the sub-array
  * @end_point: The index pointing to the last element of the sub-array
- * @n: This is the total size of the array, to be used for printing the arrays
  * Return: partition position
  */
 int lomuto_partition_scheme(int *array, int start_point, int end_point);
