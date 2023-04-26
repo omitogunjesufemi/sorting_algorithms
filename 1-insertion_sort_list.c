6153c45bc8998f1d141272afcbb7ceca8d604f30
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-04-26 23:17:39 +0100

    NULL

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -29,7 +29,7 @@ void insertion_sort_list(listint_t **head)
 {
 	listint_t *node, *tmp;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return;
 	node = (*head)->next;
 	while (node != NULL)
